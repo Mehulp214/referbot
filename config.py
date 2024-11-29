947fda34b092dd132bbc8909d2c3938450902391
Mehulp214 <114227971+Mehulp214@users.noreply.github.com>
2024-11-29 21:30:21 +0530

    Update config.py

--- config.py
+++ config.py
@@ -2,7 +2,7 @@ import os
 
 class Config:
     API_ID = int(os.getenv("API_ID", 13216322))
-    API_HASH = os.getenv("API_HASH", "15e5e632a8a0e52251ac8c3ccbe462c7"))
+    API_HASH = os.getenv("API_HASH", "15e5e632a8a0e52251ac8c3ccbe462c7")
     BOT_TOKEN = os.getenv("BOT_TOKEN", "7610980882:AAESQYI9Ca1pWSobokw1-S-QkVfTrja-Xdk")
     MONGO_URI = os.getenv("MONGO_URI", "mongodb+srv://referandearn:Qwerty_1234@cluster0.dasly.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0")
     ADMIN_IDS = list(map(int, os.getenv("ADMIN_IDS", "5993556795").split(",")))  # Admin Telegram IDs
