34c85ab4853f26ef28b87b41e2e4c481540ab1ad
Mehulp214 <114227971+Mehulp214@users.noreply.github.com>
2024-12-01 21:17:13 +0530

    Update main.py

--- main.py
+++ main.py
@@ -35,7 +35,7 @@ async def start_command(client: Client, message: Message):
                 ]
             ]
             )
-        await message.reply_text(
+            await message.reply_text(
             text = START_MSG.format(
                 first = message.from_user.first_name,
                 last = message.from_user.last_name,
