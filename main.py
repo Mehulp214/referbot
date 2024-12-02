4a559a5d1224df4578544aab22b3d7d8dc20cda5
Mehulp214 <114227971+Mehulp214@users.noreply.github.com>
2024-12-02 17:34:25 +0530

    Update main.py

--- main.py
+++ main.py
@@ -65,9 +65,10 @@ async def generate_referral_link(user_id):
     return f"https://t.me/referexamplebot?start={referral_code}"
 
 # Middleware to Enforce Subscription
-async def enforce_subscription(client: Client, message: Message):
-    if await is_subscribed(client, message.from_user.id):
-        return True
+@Client.on_message(filters.command("start") & filters.private)
+async def not_joined(client: Client, message: Message):
+    # if await is_subscribed(client, message.from_user.id):
+    #     return True
 
     buttons = []
     for channel_id in FORCE_SUB_CHANNELS:
