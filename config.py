1f930ed6c58f9fd7b85dc8e75f63ff98802287cf
Mehulp214 <114227971+Mehulp214@users.noreply.github.com>
2024-12-03 19:42:23 +0530

    Update config.py

--- config.py
+++ config.py
@@ -5,7 +5,7 @@ class Config:
     API_HASH = os.getenv("API_HASH", "15e5e632a8a0e52251ac8c3ccbe462c7")
     BOT_TOKEN = os.getenv("BOT_TOKEN", "7610980882:AAESQYI9Ca1pWSobokw1-S-QkVfTrja-Xdk")
     #MONGO_URI = os.getenv("MONGO_URI", "mongodb+srv://referandearn:Qwerty_1234@cluster0.dasly.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0")
-    MONGO_URI="mongodb+srv://new-user_31:<db_password>@cluster0.7vbmd.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"
+    MONGO_URI="mongodb+srv://new-user_31:Qwerty_1234@cluster0.7vbmd.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"
     ADMIN_IDS = list(map(int, os.getenv("ADMIN_IDS", "5993556795").split(",")))  # Admin Telegram IDs
     BOT_USERNAME = os.getenv("BOT_USERNAME", "referexamplebot")
     DEFAULT_CURRENCY = os.getenv("DEFAULT_CURRENCY", "USD")
