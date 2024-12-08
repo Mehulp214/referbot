79c44e3c64561d5302db2f30e3e41216f9e79374
Mehulp214 <114227971+Mehulp214@users.noreply.github.com>
2024-12-08 17:51:52 +0530

    Update database.py

--- database.py
+++ database.py
@@ -7,7 +7,7 @@ from config import Config
 
 # Initialize the MongoDB client and connect to the database
 dbclient = pymongo.MongoClient(Config.MONGO_URI)
-database = dbclient["REFER_START"]
+database = dbclient["REFER_START1"]
 
 # Accessing the collection 'users'
 user_data = database['users']
