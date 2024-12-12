299c73e75b41b8986c76e5806bbc95000943c349
Mehulp214 <114227971+Mehulp214@users.noreply.github.com>
2024-12-12 23:13:04 +0530

    Update start.py

--- plugins/start.py
+++ plugins/start.py
@@ -10,7 +10,7 @@ from pyrogram.enums import ChatMemberStatus
 from pyrogram.errors.exceptions.bad_request_400 import UserNotParticipant
 from pyrogram.errors import FloodWait, UserIsBlocked, InputUserDeactivated
 from pyromod.helpers import ikb
-import logging
+
  
 # from database import (
 #     user_data as ud,
@@ -39,8 +39,7 @@ from database import *
 
 
 
-logging.basicConfig(level=logging.DEBUG)  # Add logging to the plugin
-logger = logging.getLogger(__name__)
+
 
 # Bot Configurations
 # API_ID = int(os.getenv("API_ID", 13216322))
@@ -433,63 +432,6 @@ async def check_subscription_callback(client: Client, callback_query: CallbackQu
         )
 
 
-# Command to View Balance
-@app.on_message(filters.command("users") & filters.private & filters.user(ADMIN_IDS))
-async def get_users(client: Client, message: Message):
-    users = await full_userbase()
-    await message.reply(f"There are {len(users)} users using this bot.")
-
-
-# Broadcast Command
-@app.on_message(filters.command("broadcast") & filters.private & filters.user(ADMIN_IDS))
-async def broadcast_message(client: Client, message: Message):
-    if not message.reply_to_message:
-        await message.reply("Reply to a message to broadcast.")
-        return
-
-    users = await full_userbase()
-    total = len(users)
-    successful, blocked, deleted, unsuccessful = 0, 0, 0, 0
-
-    pls_wait = await message.reply("<i>Broadcasting Message... This will take some time.</i>")
-    for user_id in users:
-        try:
-            await message.reply_to_message.copy(user_id)
-            successful += 1
-        except FloodWait as e:
-            await asyncio.sleep(e.value)
-            await message.reply_to_message.copy(user_id)
-            successful += 1
-        except UserIsBlocked:
-            await del_user(user_id)
-            blocked += 1
-        except InputUserDeactivated:
-            await del_user(user_id)
-            deleted += 1
-        except Exception:
-            unsuccessful += 1
-
-    status = f"""<b>Broadcast Completed</b>
-Total: {total}
-Successful: {successful}
-Blocked: {blocked}
-Deleted: {deleted}
-Failed: {unsuccessful}"""
-    await pls_wait.edit(status)
-
-
-@app.on_message(filters.command("add_balance") & filters.private)
-async def add_command(client: Client, message: Message):
-    #user_id = message.from_user.id
-    try:
-        # Extract user_id from the command text (e.g., /refer_list 1234567890)
-        user_id = int(message.text.split()[1])
-        temp_amount = int(message.text.split()[2])
-        await update_balance(user_id, temp_amount)
-        print(user_id)
-    except (IndexError, ValueError):
-        await message.reply("Please provide a valid user ID.\nUsage: /add_balance <user_id> <amount>")
-        return
   
 
 @app.on_callback_query(filters.regex("my_referrals"))
@@ -535,50 +477,7 @@ async def my_referrals_callback(client: Client, callback_query: CallbackQuery):
 
 
 
-@app.on_message(filters.command("refer_list") & filters.user(ADMIN_IDS))
-async def refer_list_command(client: Client, message: Message):
-    # Ensure the admin is sending a valid user_id to view referrals
-    try:
-        # Extract user_id from the command text (e.g., /refer_list 1234567890)
-        user_id = int(message.text.split()[1])
-    except (IndexError, ValueError):
-        await message.reply("Please provide a valid user ID.\nUsage: /refer_list <user_id>")
-        return
 
-    # Fetch referral count for the specified user
-    ref_count = ud.count_documents({"referrer_id": user_id})
-    
-    # Fetch detailed referral information for the specified user
-    referred_users = list(ud.find({"referrer_id": user_id}))
-    referral_details = []
-
-    # Add numbering and timestamp
-    for index, user in enumerate(referred_users, 1):  # Start numbering from 1
-        referred_user_id = user['_id']
-        
-        # Fetch the referred user's name using Pyrogram
-        try:
-            user_info = await client.get_users(referred_user_id)
-            full_name = user_info.first_name + (" " + user_info.last_name if user_info.last_name else "")
-        except Exception as e:
-            full_name = "Unknown"
-        
-        # Get the timestamp from the referral document (it's in the 'referrals' array)
-        referral = next((r for r in user.get('referrals', []) if r['user_id'] == referred_user_id), None)
-        timestamp = referral.get('timestamp', 'Unknown date') if referral else 'Unknown date'
-
-        # Format the timestamp (if needed, you can convert it to a readable string)
-        referral_details.append(
-            f"{index}. User ID: `{referred_user_id}`, Name: **{full_name}**, \nReferred On: **{timestamp}**, \n[Profile Link](tg://user?id={referred_user_id})\n"
-        )
-
-    referral_details_text = "\n".join(referral_details) if referral_details else "No referrals yet."
-    f_name=user_info.first_name + (" " + user_info.last_name if user_info.last_name else "")
-    # Send the response to the admin
-    await message.reply(
-        f"Admin, you are viewing the referrals of user with ID: `{user_id}`-**{f_name}**.\n\nReferral Count: {ref_count}\n\nReferral Details:\n{referral_details_text}",
-        disable_web_page_preview=True  # Avoid link previews
-    )
 
 # Callback for Support Request
 @app.on_callback_query(filters.regex("support"))
@@ -655,26 +554,26 @@ async def admin_reply(client: Client, callback_query: CallbackQuery):
         )
 
 
-# Admin Reply Handler
-@app.on_callback_query(filters.regex(r"reply_(\d+)"))
-async def admin_reply(client: Client, callback_query: CallbackQuery):
-    user_id = int(callback_query.data.split("_")[1])
-    await callback_query.message.reply_text("Please type your reply to the user:")
+# # Admin Reply Handler
+# @app.on_callback_query(filters.regex(r"reply_(\d+)"))
+# async def admin_reply(client: Client, callback_query: CallbackQuery):
+#     user_id = int(callback_query.data.split("_")[1])
+#     await callback_query.message.reply_text("Please type your reply to the user:")
     
-    try:
-        response = await client.listen(callback_query.message.chat.id, timeout=150)  # Wait for 5 minutes
-        if response.text and response.text.lower() == "cancel":
-            await callback_query.message.reply_text("Reply canceled.")
-            return
+#     try:
+#         response = await client.listen(callback_query.message.chat.id, timeout=150)  # Wait for 5 minutes
+#         if response.text and response.text.lower() == "cancel":
+#             await callback_query.message.reply_text("Reply canceled.")
+#             return
 
-        reply_text = response.text or "No reply provided."
-        await client.send_message(
-            chat_id=user_id,
-            text=f"Admin has sent you a Reply:\n\n**{reply_text}**"
-        )
-        await callback_query.message.reply_text("Reply sent to the user successfully.")
-    except TimeoutError:
-        await callback_query.message.reply_text("Support request timed out. Please try again.")
+#         reply_text = response.text or "No reply provided."
+#         await client.send_message(
+#             chat_id=user_id,
+#             text=f"Admin has sent you a Reply:\n\n**{reply_text}**"
+#         )
+#         await callback_query.message.reply_text("Reply sent to the user successfully.")
+#     except TimeoutError:
+#         await callback_query.message.reply_text("Support request timed out. Please try again.")
  
 
 
