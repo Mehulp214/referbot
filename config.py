7c1dcc74e749b75055395a7daf0585bb2aef06c1
Mehulp214 <114227971+Mehulp214@users.noreply.github.com>
2024-12-12 22:04:47 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,13 +1,24 @@
 import os
 
-class Config:
-    API_ID = int(os.getenv("API_ID", 13216322))
-    API_HASH = os.getenv("API_HASH", "15e5e632a8a0e52251ac8c3ccbe462c7")
-    BOT_TOKEN = os.getenv("BOT_TOKEN", "7610980882:AAESQYI9Ca1pWSobokw1-S-QkVfTrja-Xdk")
-    #MONGO_URI = os.getenv("MONGO_URI", "mongodb+srv://referandearn:Qwerty_1234@cluster0.dasly.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0")
-    MONGO_URI="mongodb+srv://new-user_31:Qwerty_1234@cluster0.7vbmd.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"
-    ADMIN_IDS = list(map(int, os.getenv("ADMIN_IDS", "5993556795").split(",")))  # Admin Telegram IDs
-    BOT_USERNAME = os.getenv("BOT_USERNAME", "referexamplebot")
-    DEFAULT_CURRENCY = os.getenv("DEFAULT_CURRENCY", "USD")
-    WITHDRAWAL_CHANNEL = os.getenv("WITHDRAWAL_CHANNEL", "-1002493977004")  # Channel ID
+# class Config:
+#     API_ID = int(os.getenv("API_ID", 13216322))
+#     API_HASH = os.getenv("API_HASH", "15e5e632a8a0e52251ac8c3ccbe462c7")
+#     BOT_TOKEN = os.getenv("BOT_TOKEN", "7610980882:AAESQYI9Ca1pWSobokw1-S-QkVfTrja-Xdk")
+#     #MONGO_URI = os.getenv("MONGO_URI", "mongodb+srv://referandearn:Qwerty_1234@cluster0.dasly.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0")
+#     MONGO_URI="mongodb+srv://new-user_31:Qwerty_1234@cluster0.7vbmd.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"
+#     ADMIN_IDS = list(map(int, os.getenv("ADMIN_IDS", "5993556795").split(",")))  # Admin Telegram IDs
+#     BOT_USERNAME = os.getenv("BOT_USERNAME", "referexamplebot")
+#     DEFAULT_CURRENCY = os.getenv("DEFAULT_CURRENCY", "USD")
+#     WITHDRAWAL_CHANNEL = os.getenv("WITHDRAWAL_CHANNEL", "-1002493977004")  # Channel ID
     REFERRAL_REWARD=5
+API_ID = int(os.getenv("API_ID", 13216322))
+API_HASH = os.getenv("API_HASH", "15e5e632a8a0e52251ac8c3ccbe462c7")
+BOT_TOKEN = os.getenv("BOT_TOKEN", "7610980882:AAESQYI9Ca1pWSobokw1-S-QkVfTrja-Xdk")
+ADMIN_IDS = [5993556795]  # Replace with your Telegram User IDs
+
+FORCE_MSG = "You must join our channels to use this bot."
+START_MSG = "Welcome, {first}!"
+MAIN_MENU_MSG = "WELCOME TO MENU"
+
+# Channels for Force Subscription
+FORCE_SUB_CHANNELS = [-1002493977004]  # Add channel IDs here
