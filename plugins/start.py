d1fc5ace6c60a20d8134357fb3ed2f68a5985c82
Mehulp214 <114227971+Mehulp214@users.noreply.github.com>
2024-12-12 22:39:43 +0530

    Update start.py

--- plugins/start.py
+++ plugins/start.py
@@ -673,17 +673,7 @@ async def admin_reply(client: Client, callback_query: CallbackQuery):
         await callback_query.message.reply_text("Support request timed out. Please try again.")
  
 
-import pymongo
-import config as Config
-dbclient = pymongo.MongoClient(Config.MONGO_URI)
-database_name = dbclient["REFER_START"]
 
-@app.on_message(filters.command("drop") & filters.private)
-async def drop(client: Client, message: Message):
-    # Drop the database using the client
-    dbclient.drop_database(database_name)
-    await message.reply("Database dropped.")
-    print("DB DROPPED")
 
 
 
