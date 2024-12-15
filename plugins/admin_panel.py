126e4766ff41da50b3df53fc729d34e9652bd618
Mehulp214 <114227971+Mehulp214@users.noreply.github.com>
2024-12-15 17:33:35 +0530

    Update admin_panel.py

--- plugins/admin_panel.py
+++ plugins/admin_panel.py
@@ -1,29 +1,29 @@
-# import pymongo
-# import config as Config
-# import asyncio
-# import os
-# from datetime import datetime
-# from bot import marimo as app
-# #from config import *
-# from pyrogram.types import InlineKeyboardMarkup, InlineKeyboardButton, CallbackQuery, Message
-# from pyrogram import filters
-# from pyromod import Client
-# from pyrogram.enums import ChatMemberStatus
-# from pyrogram.errors.exceptions.bad_request_400 import UserNotParticipant
-# from pyrogram.errors import FloodWait, UserIsBlocked, InputUserDeactivated
-# from pyromod.helpers import ikb
-# from plugins.start import back_key, main_menu_callback
+import pymongo
+import config as Config
+import asyncio
+import os
+from datetime import datetime
+from bot import marimo as app
+#from config import *
+from pyrogram.types import InlineKeyboardMarkup, InlineKeyboardButton, CallbackQuery, Message
+from pyrogram import filters
+from pyromod import Client
+from pyrogram.enums import ChatMemberStatus
+from pyrogram.errors.exceptions.bad_request_400 import UserNotParticipant
+from pyrogram.errors import FloodWait, UserIsBlocked, InputUserDeactivated
+from pyromod.helpers import ikb
+from plugins.start import back_key, main_menu_callback
 
 
-# dbclient = pymongo.MongoClient(Config.MONGO_URI)
-# database_name = dbclient["REFER_START"]
+dbclient = pymongo.MongoClient(Config.MONGO_URI)
+database_name = dbclient["REFER_START"]
 
-# @app.on_message(filters.command("drop") & filters.private)
-# async def drop(client: Client, message: Message):
-#     # Drop the database using the client
-#     dbclient.drop_database(database_name)
-#     await message.reply("Database dropped.")
-#     print("DB DROPPED")
+@app.on_message(filters.command("drop") & filters.private)
+async def drop(client: Client, message: Message):
+    # Drop the database using the client
+    dbclient.drop_database(database_name)
+    await message.reply("Database dropped.")
+    print("DB DROPPED")
 
 # from config import *
 # from database import *
